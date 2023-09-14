9b17a8acf5022e0513f21c5ddc17bc12e77b465b
Franklin Marden <127708250+FranklinMSR@users.noreply.github.com>
2023-09-14 14:46:51 -0300

    AI-WEB application API

--- upload-ai-api/src/routes/generate-ai-completion.ts
+++ upload-ai-api/src/routes/generate-ai-completion.ts
@@ -1,18 +1,19 @@
 import { FastifyInstance } from "fastify";
 import { createReadStream } from 'node:fs'
 import { z } from 'zod'
+import { streamToResponse, OpenAIStream } from 'ai'
 import { prisma } from "../lib/prisma";
 import { openai } from "../lib/openai";
 
 export async function generateAICompletionRoute(app: FastifyInstance) {
-    app.post('/ai/complete', async (req) => {
+    app.post('/ai/complete', async (req, reply) => {
         const bodySchema = z.object({
             videoId: z.string().uuid(),
-            template: z.string(),
+            prompt: z.string(),
             temperature: z.number().min(0).max(1).default(0.5), 
         })
     
-        const { videoId, template, temperature } = bodySchema.parse(req.body)
+        const { videoId, prompt, temperature } = bodySchema.parse(request.body)
 
         const video = await prisma.video.findUniqueOrThrow({
             where: {
@@ -24,7 +25,7 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
             return reply.status(400).send({error: 'Video trasncription was not generated yet!'})
         }
 
-        const promptMessage = template.replace('{transcription}', video.transcription)
+        const promptMessage = prompt.replace('{transcription}', video.transcription)
 
         const response = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo-16k',
@@ -32,8 +33,17 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
             messages: [
                 { role: 'user', content: promptMessage },
             ],
+            stream: true,
         })
+            
+        const stream = OpenAIStream(response)
 
-        return response
+        streamToResponse(stream, reply.raw, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+            }, 
+        })
     })
 }
--- upload-ai-web/src/components/video-input-form.tsx
+++ upload-ai-web/src/components/video-input-form.tsx
@@ -6,9 +6,25 @@ import { Button } from "./ui/button";
 import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
 import { getFFmpeg } from "@/lib/ffmpeg";
 import { fetchFile } from '@ffmpeg/util'
+import { api } from "@/lib/axios";
 
-export function VideInputForm() {
+type Status = 'waiting' | 'converting' | 'uploading' | 'generating' | 'success'
+
+const statusMessages = {
+  converting: 'Convertendo...',
+  generating: 'Transcrevendo...',
+  uploading: 'Carregando...',
+  success: 'Sucesso!',
+}
+
+interface VideoInputFormProps{
+  onVideoUploaded: (id: string) => void
+}
+
+export function VideoInputForm(props: VideoInputFormProps) {
   const [videoFile, setVideoFile] = useState<File | null>(null)
+  const [status, setStatus] = useState<Status>('waiting')
+
   const promptInputRef = useRef<HTMLTextAreaElement>(null)
   
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
@@ -71,11 +87,29 @@ export function VideInputForm() {
       return
     }
  
-    // converter vídeo em áudio
+    setStatus('converting')
 
     const audioFile = await convertVideoToAudio(videoFile)
 
-    console.log(audioFile, prompt)
+    const data = new FormData()
+
+    data.append('file', audioFile)
+
+    setStatus('uploading')  
+
+    const response = await api.post('/videos', data)
+
+    const videoId = response.data.video.id
+
+    setStatus('generating')
+
+    await api.post(`/videos/${videoId}/transcription`,{
+      prompt,
+    })
+
+    setStatus('success')
+
+    props.onVideoUploaded(videoId)
   }
   const previewURL = useMemo(() => {
     if (!videoFile) {
@@ -108,16 +142,26 @@ export function VideInputForm() {
       <div className="space-y-2">
         <Label htmlFor="transcription_promt">Promt de transcrição</Label>
         <Textarea
-          ref={promptInputRef} 
+          ref={promptInputRef}
+          disabled={status !== 'waiting'} 
           id="transcription_promt"
           className="h-20 leading-relaxed resize-none"
           placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
         />
       </div>
 
-      <Button type="submit" className="w-full">
-        Carregar vídeo
-        <Upload className="w-4 h-4 ml-2" />
+      <Button
+        data-success={status === 'success'}
+        disabled={status !== 'waiting'}
+        type="submit"
+        className="w-full data-[success=true]:bg-emerald-400"
+      >
+        {status === 'waiting'? (
+          <>
+            Carregar vídeo
+            <Upload className="w-4 h-4 ml-2" />
+          </>
+        ) : statusMessages[status]}
       </Button>
     </form>
   );
