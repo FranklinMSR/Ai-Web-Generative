6734e4eb949b7fb67cf196fbe95d4afff44f7d1a
Franklin Marden <127708250+FranklinMSR@users.noreply.github.com>
2023-09-14 06:05:44 -0300

    Interface and API integrations

--- upload-ai-web/vite.config.ts
+++ upload-ai-web/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
