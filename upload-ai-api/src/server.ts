4e1d4e12587e9d31fe5e745183ad00bb2e1c8f0d
Franklin Marden <127708250+FranklinMSR@users.noreply.github.com>
2023-09-26 20:48:02 -0300

    Adjustments for deploy

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -18,7 +18,8 @@ app.register(generateAICompletionRoute)
 
 
 app.listen({
-    port: 3333,
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
 }).then(() => {
     console.log('HTTP Server Running!')
 })
